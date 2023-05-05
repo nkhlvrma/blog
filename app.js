7dc75f1d906f2a88bba62eb1f4c0562367b30393
nkhlvrma <nkhlvrma@gmail.com>
2023-05-05 14:14:20 +0530

    Update Code

--- app.js
+++ app.js
@@ -15,8 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res){
-  res.render("home", {homeContent: homeStartingContent});
+  res.render("home", {
+    homeContent: homeStartingContent, 
+    posts: posts
+  });
 });
 
 app.get("/about", function(req, res) {
@@ -32,10 +37,20 @@ app.get("/compose", function(req, res) {
 })
 
 app.post("/compose", function(req, res){
-  console.log(req.body.postTitle);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  }
+
+  posts.push(post);
+  
+  res.redirect("/");
+
 });
 
-
+app.get("/posts/:postName", function (req, res){
+  console.log(req.params.postName);
+});
 
 
 
